9de25cff94667435c045ca2ff96c5a24b2261934
Adin W-T <adinrwt@gmail.com>
2022-02-23 14:00:05 -0500

    Format json data

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,21 +1,12 @@
 import requests
 import json
 
-current_comic_res = requests.get("https://xkcd.com/info.0.json")
-current_comic_json = json.loads(current_comic_res.content.decode("ascii"))
-current_comic_num = current_comic_json["num"]
+unformatted_file = open("xkcd.json", "rt")
+unformatted_content = "".join(unformatted_file.readlines())
+unformatted_file.close()
 
-data_file = open("data.json", "at+")
-data_file.write("[\n")
+unformatted_json = json.loads(unformatted_content)
 
-for comic_num in range(0, current_comic_num + 1):
-    url = "https://xkcd.com/{}/info.0.json".format(comic_num)
-    comic_res = requests.get(url)
-
-    if comic_res.status_code == 200:
-        data_file.write(comic_res.content.decode("ascii") + ",\n")
-    else:
-        print("{}: {}".format(url, comic_res.status_code))
-
-data_file.write("]")
-data_file.close()
+formatted_file = open("xkcd_formatted.json", "wt+")
+formatted_file.write(json.dumps(parsed_json, indent=4))
+formatted_file.close()
