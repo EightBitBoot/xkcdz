09699608ad170b2e9e4357e90ad3f999f7c3250b
Adin W-T <adinrwt@gmail.com>
2022-02-27 23:49:36 -0500

    Add Discord gateway capabilities
    The bot client currently just sends heartbeats according to Discord's
    perscribed schedule and prints all incoming messages from the gateway.
    
    PrintThread is just a test of using multithreading alongside asyncio.

---
+++ README.txt
@@ -0,0 +1,16 @@
+Discord Bot
+-----------
+   Requires python >= 3.7
+
+   Setup
+   -----
+   1) Create a virtual environment for the bot (eg. "python -m venv venv")
+   2) Install the requirements in requirements.txt
+   3) Create a file in discord_bot/ called secrets.hjson containing two keys: "bot_key" and "bot_name" with
+      the corresponding values.  Bot name is just used in the identify payload sent to Discord. 
+
+   Reference
+   ---------
+   https://hjson.github.io/
+
+A better readme is coming in the future
--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,9 +1,115 @@
 import requests
 import json
-import io
+import hjson
+from threading import Thread
+import time
+import websockets
+import asyncio
 
-with open("xkcd.json", "r+") as unformatted_file:
-    json_data = json.loads("".join(unformatted_file.readlines()))
-    unformatted_file.truncate(0)
-    unformatted_file.seek(0, io.SEEK_SET)
-    unformatted_file.write(json.dumps(json_data))
+class PrintThread(Thread):
+    def run(self):
+        then = time.perf_counter_ns()
+        now = time.perf_counter_ns()
+        while True:
+            print("Time Slept: {}".format(now - then))
+            then = time.perf_counter()
+            time.sleep(1.0)
+            now = time.perf_counter()
+
+
+BASE_DISCORD_URL = "https://discord.com/api"
+DISCORD_URL_V9 = BASE_DISCORD_URL + "/v9"
+
+last_sequence = None
+
+class DiscordWebsocketClient:
+    def __init__(self, websocket_client):
+        self._last_sequence = None
+        self._is_closed = False
+        self.websocket_client = websocket_client
+
+    async def recv(self, *args, **kwargs):
+        raw_result = await self.websocket_client.recv(*args, **kwargs)
+
+        json_result = json.loads(raw_result)
+        if "s" in json_result:
+            self._last_sequence = json_result["s"]
+
+        return raw_result
+
+    async def send(self, *args, **kwargs):
+        return await self.websocket_client.send(*args, **kwargs)
+
+    async def close(self, *args, **kwargs):
+        self._is_closed = True
+        return await self.websocket_client.close()
+
+    async def send_heartbeat_task(self, interval):
+        while not self._is_closed:
+            then = time.perf_counter()
+            await asyncio.sleep(interval / 1000)
+            if self._is_closed:
+                break
+
+            now = time.perf_counter()
+            await self.send(json.dumps({"op": 1, "d": self._last_sequence}))
+            print("Heartbeat sent after {}ms; interval={}ms".format((now - then) * 1000, interval))
+
+
+async def heartbeat(websocket_client, interval):
+    while True:
+        await asyncio.sleep(interval / 1000)
+        await websocket_client.send(json.dumps({"op": 1, "d": last_sequence}))
+        print("Heartbeat")
+
+
+async def main():
+    json_data = None
+    with open("xkcd.json", "r") as data_file:
+        json_data = json.loads("".join(data_file.readlines()))
+
+    bot_secrets = None
+    with open("secrets.hjson", "r") as secrets_file:
+        bot_secrets = hjson.loads("".join(secrets_file.readlines()))
+
+    bot_token = bot_secrets["bot_token"]
+    bot_name = bot_secrets["bot_name"]
+
+    gateway_json = requests.get(DISCORD_URL_V9 + "/gateway/bot", headers={"Authorization": "Bot " + bot_token}).json()
+    print(json.dumps(gateway_json, indent=4), end="\n\n")
+
+    websocket_client = await websockets.connect(gateway_json["url"] + "/?v=9&encoding=json", ping_interval=None, ping_timeout=None, max_queue=None, max_size=None)
+    discord_websocket_client = DiscordWebsocketClient(websocket_client)
+    hello_json = json.loads(await discord_websocket_client.recv())
+    print(json.dumps(hello_json, indent=4), end="\n\n")
+
+    heartbeat_task = asyncio.create_task(discord_websocket_client.send_heartbeat_task(hello_json["d"]["heartbeat_interval"]))
+
+    identify_payload = {
+        "op": 2,
+        "d": {
+            "token": bot_token,
+            "properties": {
+                "$os": "linux",
+                "$browser": bot_name,
+                "$device": bot_name
+            },
+            "intents": 1
+        }
+    }
+    await discord_websocket_client.send(json.dumps(identify_payload))
+
+    try:
+        while True:
+            raw_event = await discord_websocket_client.recv()
+            print(json.dumps(json.loads(raw_event), indent=4), end="\n\n")
+    except KeyboardInterrupt as e:
+        print("Interrupted")
+
+    await discord_websocket_client.close()
+    
+
+if __name__ == "__main__":
+    print_thread = PrintThread()
+    # print_thread.start()
+    asyncio.run(main())
