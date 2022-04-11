c1a11c15eedca755cfba95815a7b47c88c95d6ec
Adin W-T <adinrwt@gmail.com>
2022-04-11 07:21:41 -0400

    Flush out rudimentary xkcd api lib

--- lib/xkcd/xkcd.py
+++ lib/xkcd/xkcd.py
@@ -1,32 +1,54 @@
 
+from typing import Tuple, Optional
+
+import requests
+import json
+
+from json_objects import json_obj
+
 __all__ = [
     "XKCDComic"
 ]
 
-class XKCDComic:
-    def __init__(self, num, img_url, safe_title, alt, day, month, year, title, news, link, transcript):
-        self.num = num
-        self.img_url = img_url
-        self.safe_title = safe_title
-        self.alt = alt
-        self.day = day
-        self.month = month
-        self.year = year
-        self.title = title
-        self.news = news
-        self.link = link
-        self.transcript = transcript
+XKCD_LATEST_URL = "https://xkcd.com/info.0.json"
+XKCD_COMIC_NUM_URL = "https://xkcd.com/{}/info.0.json"
 
-    @classmethod
-    def from_parsed_json(cls, parsed_json):
-        num        = parsed_json["num"]        if "num"        in parsed_json else None   
-        img_url    = parsed_json["img_url"]    if "img_url"    in parsed_json else None   
-        safe_title = parsed_json["safe_title"] if "safe_title" in parsed_json else None   
-        alt        = parsed_json["alt"]        if "alt"        in parsed_json else None   
-        day        = parsed_json["day"]        if "day"        in parsed_json else None   
-        month      = parsed_json["month"]      if "month"      in parsed_json else None   
-        year       = parsed_json["year"]       if "year"       in parsed_json else None   
-        title      = parsed_json["title"]      if "title"      in parsed_json else None   
-        news       = parsed_json["news"]       if "news"       in parsed_json else None   
-        link       = parsed_json["link"]       if "link"       in parsed_json else None   
-        transcript = parsed_json["transcript"] if "transcript" in parsed_json else None   
+@json_obj
+class XKCDComic:
+    num: int
+    img_url: str
+    safe_title: str
+    alt: str
+    day: int
+    month: int
+    year: int
+    title: str
+    news: str
+    link: str
+    transcript: str
+    extra_parts: dict
+
+    def to_json(self):
+        # "Override" to omit extra_parts if it is None
+        json_dict = self.to_json_dict()
+        
+        if json_dict["extra_parts"] is None:
+            del json_dict["extra_parts"]
+
+        return json.dumps(json_dict)
+
+
+def get_comic(num: Optional[int] = None) -> Tuple[Optional[XKCDComic], int]:
+    comic = None
+    response = None
+
+    if num:
+        response = requests.get(XKCD_COMIC_NUM_URL.format(num))
+    else:
+        response = requests.get(XKCD_LATEST_URL)
+    
+
+    if(response.status_code == 200):
+        comic = XKCDComic.from_json_str(response.content)
+
+    return (comic, response.status_code)
